caaef363cb765cd814a05c00629701e6885059bd
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-04-22 09:59:23 -0700

    put and delete in process

--- public/javascripts/newMessage.js
+++ public/javascripts/newMessage.js
@@ -4,6 +4,8 @@ $(document).ready(init);
 
 function init(){
   $("#submit").on("click", newMessage);
+  $("#edit").on("click", editMessage);
+  $("#delete").on("click", removeMessage);
 }
 
 function newMessage(e){
@@ -15,6 +17,34 @@ function newMessage(e){
   var newMessage = {name: contactName, email: email, phone: phone, notes: notes};
 
   $.post('/messages/add', newMessage)
+    .success(function(data){
+      window.location = "/messages";
+    })
+}
+
+function editMessage(e){
+  e.preventDefault();
+  var contactName = $('#name').val();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
+  var notes = $('#notes').val();
+  var newMessage = {name: contactName, email: email, phone: phone, notes: notes};
+
+  $.put('/messages/update', newMessage)
+    .success(function(data){
+      window.location = "/messages";
+    })
+}
+
+function removeMessage(e){
+  e.preventDefault();
+  var contactName = $('#name').val();
+  var email = $('#email').val();
+  var phone = $('#phone').val();
+  var notes = $('#notes').val();
+  var newMessage = {name: contactName, email: email, phone: phone, notes: notes};
+
+  $.delete('/messages/delete', deMessage)
     .success(function(data){
       window.location = "/messages";
     })
